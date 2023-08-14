08fe258483e4e4066068d4f539351f2fb7fb162c
Ernest Prabhakar <ernest@quiltdata.io>
2023-08-14 10:47:15 -0700

    refactor upload.py

--- upload.py
+++ upload.py
@@ -1,4 +1,15 @@
 import quilt3
 
+REGISTRY = "s3://quilt-example"
+PKG_NAME = "examples/benchling-packager"
+TEMPLATE = "benchling_packager.yaml"
+TARGET = f"build/{TEMPLATE}"
+
+def upload():
+    pkg = quilt3.Package()
+    pkg.set("README.md", "Install.md")
+    pkg.set(TEMPLATE, TARGET)
+    pkg.push(PKG_NAME, registry=REGISTRY)
+
 if __name__ == "__main__":
-    quilt3.Package().set('README.md', 'install.md').set('benchling_packager.yaml', 'build/benchling_packager.yaml').push('examples/benchling-packager', 's3://quilt-example')
+    upload()
